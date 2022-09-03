57f57cff4353bb2bdb1f536e19fced6831066f9f
iverly <github@iverly.net>
2022-09-03 11:36:54 +0200

    feat(app): add env_logger crate
    Signed-off-by: iverly <github@iverly.net>

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+
+    log::info!(target: "kubecraft-proxy", "starting up");
+
+    // todo(iverly): add logic here
+
+    log::info!(target: "kubecraft-proxy", "shutting down");
 }
